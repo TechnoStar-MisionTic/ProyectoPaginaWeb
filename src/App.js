1f7c30cdc0902200af543bb2e4b23f2eb52fe432
Andrés Niño <Felipe>
2021-09-30 21:22:02 -0500

    React cambios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
+import logo from "./media/images/logo.jpg"
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="inicio-box">
+        <img className="logo" src={logo} alt="Logo"/>
+        <h2> Imperio Musical</h2>
+        <label for="E-mail">Usuario</label>
+        <input type="text" placeholder="Usuario" />
+        <label for="password">Contraseña</label>
+        <input type="password" placeholder="Contraseña" />
+        <input type="submit" value="ingresar" />
+        <input type="submit" value="Ingresa con google" />
+        <a href="https://www.google.com/?hl=es" target="_blank"></a>
+        <a href="#">¿Olvidaste tu contraseña?</a> <br />
+        <a href="#">Registrate</a>
+      </div>
     </div>
   );
 }
