d303eafdb47a32d9ece593a36b71f187c83beb08
Andrés Niño <Felipe>
2021-10-05 16:02:49 -0500

    Gestion roles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,18 +3,18 @@ import logo from "../media/images/logo.jpg"
 function Login(){
     return(
         <div className="inicio-box">
-        <img className="logo" src={logo} alt="Logo"/>
-        <h2> Imperio Musical</h2>
-        <label for="E-mail">Usuario</label>
-        <input type="text" placeholder="Usuario" />
-        <label for="password">Contraseña</label>
-        <input type="password" placeholder="Contraseña" />
-        <input type="submit" value="ingresar" />
-        <input type="submit" value="Ingresa con google" />
-        <a href="https://www.google.com/?hl=es" target="_blank"></a>
-        <a href="#">¿Olvidaste tu contraseña?</a> <br />
-        <a href="#">Registrate</a>
-      </div>
+          <img className="logo" src={logo} alt="Logo"/>
+          <h2> Imperio Musical</h2>
+          <label for="E-mail">Usuario</label>
+          <input type="text" placeholder="Usuario" />
+          <label for="password">Contraseña</label>
+          <input type="password" placeholder="Contraseña" />
+          <input type="submit" value="ingresar" />
+          <input type="submit" value="Ingresa con google" />
+          <a href="https://www.google.com/?hl=es" target="_blank"></a>
+          <a href="#">¿Olvidaste tu contraseña?</a> <br />
+          <a href="#">Registrate</a>
+        </div>
     )
 }
 
---
+++ src/pages/Roles.jsx
@@ -0,0 +1,33 @@
+import NavBar from "../components/NavBar"
+
+function Roles(){
+    return(
+        <div>
+            <header>
+                <NavBar />
+            </header>
+            <main>
+                <form >
+                    <div className="roles">
+                        <h2>Gestion De Roles</h2>
+                        <input type="text" placeholder="Usuario" />
+                        <h2>Datos Del Usuario</h2>
+                        <input type="text" placeholder="Nombre" disabled/>
+                        <input type="email" placeholder="Correo" disabled/> <br/>
+                        <input type="text" placeholder="Privilegios"disabled/>
+                        <div className="flexing">
+                            <select name="Estado">
+                                <option value="Administrador">Administrador</option>
+                                <option value="Vendedor">Vendedor</option>
+                                <option value="Visitante">Visitante</option>
+                            </select>
+                            <input type="submit" value="Enviar"/>
+                        </div>
+                    </div>
+                </form>
+            </main>
+        </div>
+    )
+}
+
+export default Roles;
